018b9dd66e2b8449c3a84ddbbbdded63172067be
Igor Dias <igor.g.dias@aluno.senai.br>
2024-08-21 16:57:05 -0300

    JavaScript fundamentos

---
+++ aula9js/script.js
@@ -0,0 +1,149 @@
+//Mostra um alerta na página
+// alert("Outra mensagem")
+
+//Console
+/*console.log("Olha a mensagem")
+console.log("Olha a mensagem o retorno")
+
+var texto = "Taylor Swift"
+var numero = 1989
+var theBest = true
+var nulo = null
+var indefinido
+
+console.log(texto)
+console.log(numero)
+console.log(theBest)
+console.log(nulo)
+console.log(indefinido)
+
+console.log(typeof(texto))
+console.log(typeof(numero))
+console.log(typeof(theBest))
+console.log(typeof(nulo))
+console.log(typeof(indefinido))
+*/
+
+//Operadores
+//Aritméticos: +-/*%
+/*var a = 10
+var b = 3
+
+console.log("Soma:", a+b)
+console.log("Subtração:", a-b)
+console.log("Multiplicação:", a*b)
+console.log("Divisão:", a/b)
+console.log("Módulo:", a%b)
+*/
+
+//Lógicos: &&(and), ||(or), !(not)
+/*var verdade = true
+var mentira = false
+
+console.log(verdade && mentira)
+console.log(verdade || mentira)
+console.log(verdade && !mentira)
+*/
+
+//Relacionais: >,<, >=, <=, ==(comparando), !=(diferente), ===(comparar valor e tipo), !==(diferente valor e tipo)
+/*var a = 10
+var b = 5
+var c = "10"
+
+console.log("Relacionais")
+console.log("10 > 5: ", a > b)
+console.log("10 < 5: ", a < b)
+console.log("10 >= 5: ",a >= b)
+console.log("10 <= 5: ", a <= b)
+console.log("10 == 5:", a == b)
+console.log("10 != 5: ", a != b)
+console.log("10 == 10: ", a == c)
+console.log("10 === 10: ", a === c)
+console.log("10 !== 10: ", a !== c)
+*/
+
+//Condicionais
+//if
+/*var estaVivo = true
+if(estaVivo){
+    console.log("Está VIVO");
+}
+else if(!estaVivo){
+    console.log("Está MORTO");
+
+}
+else{
+    console.log("Não ta NADA");
+    
+}
+*/
+
+//Switch
+/*var camisa = "Preta"
+
+switch(camisa){
+    case "Azul":
+        console.log("A camisa é azul");
+        break
+    
+    case "Amarela":
+        console.log("A camisa é amarela")
+        break
+
+    case "Preta":
+        console.log("A camisa é preta")
+        break
+
+    default:
+        console.log("A camisa é sei lá")
+        break
+}
+*/
+
+//Loops
+//For
+/*for(var i = 0; i < 5; i++){
+    console.log("Estou aqui", i)
+}
+    console.log("Acabou o FOR")
+*/
+
+//While
+/*var it = 1
+while (it < 10) {
+    console.log("Estou aqui 2 - O Retorno", it)
+    // console.log(it)
+    it++
+}
+*/
+
+//Funções
+/*function teste1(){
+    console.log("Estou aqui na função 1")
+}
+
+function teste2(){
+    console.log("Agora estou na função 2")
+}
+
+teste1()
+teste2()
+*/
+
+//Interação com o usuário via prompt
+
+/*var resposta = prompt("Digite um algo")
+var resposta2 = prompt("Digite um algo")
+
+console.log(resposta, resposta2)
+alert(resposta)
+alert(resposta2)
+*/
+
+/*var primeiroNumero = prompt("Digite o primeiro número")
+var segundoNumero = prompt("Digite o segundo número")
+var resultado = Number(primeiroNumero) + Number(segundoNumero)
+
+console.log(resultado)
+alert(resultado)
+*/
